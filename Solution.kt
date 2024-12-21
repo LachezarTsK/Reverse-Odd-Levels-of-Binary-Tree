3660daca80bc95f2d6b23145401914b02b3dd51e
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-12-21 22:10:22 +0100

    Update Solution.kt

--- Solution.kt
+++ Solution.kt
@@ -1,7 +1,8 @@
 
 class Solution {
 
-    fun reverseOddLevels(root: TreeNode?): TreeNode? {        
+    fun reverseOddLevels(root: TreeNode?): TreeNode? {
+
         depthFirstSearchByPairingSymetricTreeNodesPerLevel(root?.left, root?.right, 1)
         return root
     }
@@ -26,4 +27,13 @@ class Solution {
         left.`val` = right.`val`
         right.`val` = leftValue
     }
+}
+
+/*
+Class TreeNode is in-built in the solution file on leetcode.com.
+When running the code on the website, do not include this class.
+ */
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
 }
